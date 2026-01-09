08a925697c3a00da2312cf88aedef02d81d2ed5d
Keri Wilkins <131410728+flowerbombs@users.noreply.github.com>
2026-01-08 22:11:50 -0500

    Update create_ed_stays_summary_view.sql

--- create_ed_stays_summary_view.sql
+++ create_ed_stays_summary_view.sql
@@ -3,7 +3,7 @@
 
 #Creates a base table (view) with columns from ED_Stays and Triage_Assesments using a 
 left join. DATEDIFF is used to create column ed_los_minutes from intime and outtime. 
-CREATE VIEW ED_STAYS_SUMMARY AS
+CREATE VIEW ED_Stays_Summary AS
 SELECT
 	ED_Stays.stay_id,
 	ED_Stays.subject_id,
@@ -33,5 +33,30 @@ SELECT
     SUM(CASE WHEN ed_los_minutes IS NULL THEN 1 ELSE 0 END) AS null_ed_los
 FROM ED_Stays_Summary;
 
-
+#Altering ED_Stays_Summary by adding columns, gender, race, arrival transport, from ED_Stays and columns
+temperature, heartrate, resprate, o2sat, sbp, dbp from Triage_Assessments. 
+CREATE OR ALTER VIEW ED_Stays_Summary AS
+SELECT
+	ED_Stays.stay_id,
+	ED_Stays.subject_id,
+	ED_Stays.intime,
+	ED_Stays.outtime,
+	DATEDIFF(MINUTE, ED_Stays.intime, ED_Stays.outtime) AS ed_los_minutes,
+	ED_Stays.gender,
+	ED_Stays.race,
+	ED_Stays.arrival_transport AS arrival_transportation,
+	ED_Stays.disposition,
+	Triage_Assessments.acuity,
+	Triage_Assessments.chiefcomplaint,
+	Triage_Assessments.pain_score,
+	Triage_Assessments.temperature,
+	Triage_Assessments.heartrate AS triage_heart_rate,
+	Triage_Assessments.resprate AS respiratory_rate,
+	Triage_Assessments.o2sat AS oxygen_saturation,
+	Triage_Assessments.sbp AS systolic_bp,
+	Triage_Assessments.dbp AS diastolic_bp.
+	Triage_Assessments.pain
+FROM ED_Stays
+LEFT JOIN Triage_Assessments
+	ON ED_Stays.stay_id = Triage_Assessments.stay_id;
 
