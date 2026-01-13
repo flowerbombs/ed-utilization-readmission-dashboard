22e13976ece64c7d8237fc68b8702c57cb2257b7
Keri Wilkins <131410728+flowerbombs@users.noreply.github.com>
2026-01-13 16:20:45 -0500

    Update #create_ED_Encounter_Analytics.sql

--- #create_ED_Encounter_Analytics.sql
+++ #create_ED_Encounter_Analytics.sql
@@ -1,9 +1,15 @@
 #Author: Keri W.
 #Date: 12 January 2026
-  
-#Create ED_Encounter_Analytics view by using a left join on views ED_Stays_Summary, ED_Revisits_Summary, and ED_Primary_Diagnosis on primary key
-stay_id. ED_Encounter_Analytics represents one row per ED stay, enriched with revisit metrics and primary diagnosis information when available,
-and is intended for analytics and dashboard reporting. 
+
+#Sanity check in preparation of a left join between ED_Stays_Summary and ED_Revistis_Summary on stay_id and
+first_stay_id.
+SELECT COUNT(first_stay_id) AS first_stay_id_counts
+	FROM ED_Revisits_Summary;
+
+#Create ED_Encounter_Analytics view by using a left join on views ED_Stays_Summary, ED_Revisits_Summary, 
+and ED_Primary_Diagnosis on primary key stay_id. ED_Encounter_Analytics represents one row per ED stay, 
+enriched with revisit metrics and primary diagnosis information when available, and is intended for analytics
+and dashboard reporting. 
 CREATE OR ALTER VIEW ED_Encounter_Analytics AS
 SELECT
   ED_Stays_Summary.stay_id,
@@ -30,9 +36,9 @@ SELECT
   ED_Revisits_Summary.total_future_ed_visits,
   ED_Revisits_Summary.revisit_72hr_flag,
   ED_Revisits_Summary.revisit_7day_flag,
-  ED_Primary_Diagnosis.icd_code,
-  ED_Primary_Diagnosis.icd_title
+  ED_Primary_Diagnosis_Summary.icd_code,
+  ED_Primary_Diagnosis_Summary.icd_title
   
 FROM ED_Stays_Summary 
 LEFT JOIN  ED_Revisits_Summary ON ED_Stays_Summary.stay_id =  ED_Revisits_Summary.stay_id
-LEFT JOIN  ED_Primary_Diagnosis ON ED_Stays_Summary.stay_id = ED_Primary_Diagnosis.stay_id;
+LEFT JOIN  ED_Primary_Diagnosis_Summary ON ED_Stays_Summary.stay_id = ED_Primary_Diagnosis_Summary.stay_id;
